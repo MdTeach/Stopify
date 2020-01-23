eb8a991c8c92598c275a1b0e90f532aa1420d2ee
PMP56 <prashannapaudel2@gmail.com>
2020-01-23 16:20:11 +0545

    Basic Sidebar done. No routes

--- src/components/user/music_player/side_bar/side_bar.js
+++ src/components/user/music_player/side_bar/side_bar.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
-import './side_bar.css'
+import Home from "./buttons/HomeB";
+import Search from "./buttons/Search";
+import SongsLib from "./buttons/songsLib";
+import "./side_bar.css";
 
-export default ()=>{
-    return(
-        <div>This is side_bar</div>
-    )
-}
+export default () => {
+  const [currentPage, setCurrentPage] = useState("Home");
+
+  const updatePage = page => {
+    setCurrentPage(page);
+  };
+  return (
+    <nav className="sidenav">
+      <div className="head">
+        <ul className="items">
+          <li onClick={() => updatePage("Home")}>
+            <Home page={currentPage} />
+          </li>
+          <li onClick={() => updatePage("Search")}>
+            <Search page={currentPage} />
+          </li>
+          <li onClick={() => updatePage("Library")}>
+            <SongsLib page={currentPage} />
+          </li>
+        </ul>
+      </div>
+      <div>Playlist</div>
+    </nav>
+  );
+};

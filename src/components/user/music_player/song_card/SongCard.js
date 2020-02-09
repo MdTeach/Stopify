b14820dbd31f5c32d731cb634c89f6e666daa896
PMP56 <prashannapaudel2@gmail.com>
2020-02-10 00:05:00 +0545

    Song card text managed

--- src/components/user/music_player/song_card/SongCard.js
+++ src/components/user/music_player/song_card/SongCard.js
@@ -1,46 +1,45 @@
 import React from "react";
 import "./SongCard.css";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-import PlayPauseButton from './button/Button';
+import PlayPauseButton from "./button/Button";
 
-var data=[];
+var data = [];
 
-export const cardInfo=()=>{
-  return data; 
-}
-
+export const cardInfo = () => {
+  return data;
+};
 
 export default props => {
-
-  const cardDetails=()=>{
-    data=[];
+  const cardDetails = () => {
+    data = [];
     data.push(props.data);
-  }
-  
+  };
+
   const updateCurrentMusic = () => {
     props.changeMusic(props.data);
   };
-  
- 
-
 
   return (
     <div className="song-card">
-      <Link to={'/album/'+props.data.name} style={{textDecoration:'none'}}>
+      <Link to={"/album/" + props.data.name} style={{ textDecoration: "none" }}>
         <div onClick={cardDetails}>
-      <img
-        className="song-image"
-        src={props.data.imageUrl}
-        alt={props.data.imageUrl}
-      />
-      <div className="song-name">{props.data.name}</div>
-      <div className="song-author">{props.data.artist}</div>
-      </div>
+          <img
+            className="song-image"
+            src={props.data.imageUrl}
+            alt={props.data.imageUrl}
+          />
+          <div className="song-name">
+            {props.data.name.length >= 20
+              ? props.data.name.substr(0, 20) + "..."
+              : props.data.name}
+          </div>
+          <div className="song-author">{props.data.artist}</div>
+        </div>
       </Link>
-      <PlayPauseButton 
+      <PlayPauseButton
         updateCurrentMusic={updateCurrentMusic}
-        isPlaying = {props.data === props.currentPlaying }
+        isPlaying={props.data === props.currentPlaying}
       />
     </div>
   );

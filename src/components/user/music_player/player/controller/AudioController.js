eeb2f449aacad2c5b46fdb43cab3e464fd294bb2
Abishek Bashyal <abishek_bashyal@outlook.com>
2020-02-10 17:52:09 +0545

    muisc player integrated

--- src/components/user/music_player/player/controller/AudioController.js
+++ src/components/user/music_player/player/controller/AudioController.js
@@ -1,14 +1,16 @@
 import React from 'react'
 
 import './AudioController.css'
-
+import MusicPlayer from './music_player/MusicPlayer'
 export default (props)=>{
     const data = props.data;
     const isDataAvialbel = Object.keys(data).length === 0 ? false : true
     return(
         
         <div className="audio-controller">
-            {(isDataAvialbel) ? "Play "+data["audioUrl"] :""}
+            {/* {(isDataAvialbel) ? "Play "+data["audioUrl"] :""} */}
+            <MusicPlayer audioUrl={data["audioUrl"]}/>
         </div>
+        
     );
 }

a1d6cd8ab7cb9d820f3304a7dde9d1b7993529fc
PMP56 <prashannapaudel2@gmail.com>
2020-02-21 23:25:41 +0545

    Music Player Improvised

--- src/components/user/music_player/player/controller/music_player/audio_controls/AudioControls.js
+++ src/components/user/music_player/player/controller/music_player/audio_controls/AudioControls.js
@@ -1,61 +1,72 @@
-import React from 'react'
+import React from "react";
 
 //material ui
 
 //play pause
-import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
-import PauseCircleOutlineIcon from '@material-ui/icons/PauseCircleOutline';
+import PlayCircleOutlineRoundedIcon from "@material-ui/icons/PlayCircleOutline";
+import PauseCircleOutlineRoundedIcon from "@material-ui/icons/PauseCircleOutline";
 
 //skip next
-import SkipNextIcon from '@material-ui/icons/SkipNext';
-import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
 
 //shuffle button
-import ShuffleIcon from '@material-ui/icons/Shuffle';
+import ShuffleIcon from "@material-ui/icons/Shuffle";
 
 //loop button
-import LoopIcon from '@material-ui/icons/Loop';
+import LoopIcon from "@material-ui/icons/Loop";
 
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
-
-import './AudioControls.css'
+import "./AudioControls.css";
 
 //defining the styles for the button
 const useStyles = makeStyles({
-    Button: {
-        marginLeft:".25em",
-        marginRight:".25em",
-        color: "#b3b3b3",
-        fontSize: "1.8em",
-        '&:hover':{
-            color:"white"
-        }
-    },
-
-
-})
-
-export default (props)=>{
-    //for the styling
-    const classes = useStyles();
-
-    const handlePlayPause = ()=>{
-        props.handlePlayPause()
+  Button: {
+    marginLeft: ".25em",
+    marginRight: ".25em",
+    color: "#b3b3b3",
+    fontSize: "1.6em",
+    "&:hover": {
+      color: "white"
     }
+  },
+  ppButton: {
+    marginLeft: ".25em",
+    marginRight: ".25em",
+    color: "#b3b3b3",
+    fontSize: "3em",
+    "&:hover": {
+      color: "white"
+    }
+  }
+});
 
-    return(
-        <div className="control-buttons">
-            <ShuffleIcon className={classes.Button}/>
-            <SkipPreviousIcon className={classes.Button}/>
-            {
-            (props.isPaused)?
-                <PlayCircleOutlineIcon className={classes.Button} onClick={handlePlayPause}/>
-            :   
-                <PauseCircleOutlineIcon className={classes.Button} onClick={handlePlayPause}/>    
-            }
-            <SkipNextIcon className={classes.Button}/>
-            <LoopIcon className={classes.Button}/>
-        </div>
-    )
-}
+export default props => {
+  //for the styling
+  const classes = useStyles();
+
+  const handlePlayPause = () => {
+    props.handlePlayPause();
+  };
+
+  return (
+    <div className="control-buttons">
+      <ShuffleIcon className={classes.Button} />
+      <SkipPreviousIcon className={classes.Button} />
+      {props.isPaused ? (
+        <PlayCircleOutlineRoundedIcon
+          className={classes.ppButton}
+          onClick={handlePlayPause}
+        />
+      ) : (
+        <PauseCircleOutlineRoundedIcon
+          className={classes.ppButton}
+          onClick={handlePlayPause}
+        />
+      )}
+      <SkipNextIcon className={classes.Button} />
+      <LoopIcon className={classes.Button} />
+    </div>
+  );
+};

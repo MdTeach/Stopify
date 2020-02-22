91d8467a1579f7037d769e19b4a5d55444996fe1
Abishek Bashyal <abishek_bashyal@outlook.com>
2020-02-22 15:43:09 +0545

    Styles updated on the search

--- src/components/user/music_player/search/Search.js
+++ src/components/user/music_player/search/Search.js
@@ -5,6 +5,43 @@ import HorizontalContainer from '../horizontal_music_container/HorizontalMusicCo
 
 //css
 import './Search.css'
+import { fade, makeStyles } from '@material-ui/core/styles';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+
+
+const useStyles = makeStyles(theme => ({
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: "#ffffff",
+        marginRight: theme.spacing(2),
+        marginLeft: 0,
+        width: '6   0%',
+        fontSize:"3em",
+    },
+    searchIcon: {
+        width: theme.spacing(7),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize:"3em",
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 7),
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        fontSize:"3em",
+    },
+}))
+
+
 
 export default (props)=>{
     const allSongs = props.allSongs
@@ -26,6 +63,7 @@ export default (props)=>{
             setsongArrayByNames(nameFilter)
             
             //filter the artist name
+            console.log(setsongArrayByArtist)
             const artistFilter = allSongs.filter(
                 el => el["artist"].toLowerCase().search(q) > -1
             );
@@ -33,14 +71,31 @@ export default (props)=>{
         }
     }
 
+    const classes = useStyles();
+
+
     return(
-        <div className="SearchWrapper" onChange={(e)=>{handleSearch(e)}}>
-            <div>Search Here</div>
-            <input type="text" value={currentQuery}/>
+        <div className="SearchWrapper" >
+            <div className={classes.search}>
+                <div className={classes.searchIcon}>
+                    <SearchIcon />
+                </div>
+                <InputBase
+                    placeholder="Search …"
+                    classes={{
+                        root: classes.inputRoot,
+                        input: classes.inputInput,
+                    }}
+                    inputProps={{ 'aria-label': 'search' }}
+                    value={currentQuery}
+                    onInput={(e)=>{handleSearch(e)}}
+                    value={currentQuery}
+                />
+            </div>
             
             {
                 (currentQuery.trim() == "") ? 
-                    <div><h3>Search for artist, song</h3></div>
+                    <div className="notFoundLabel"><h1>Search for artist, song</h1></div>
                     :
                     (songArrayByNames.length > 0 || songArrayByNames > 0)?
                         <div>
@@ -48,7 +103,10 @@ export default (props)=>{
                         <HorizontalContainer data={songArrayByArtist} title={"By Artist"}/>
                         </div>
                     :
-                    <div><h3>No results found for {currentQuery}</h3></div>
+                    <div className="notFoundLabel">
+                        <h1>No results found for {currentQuery}</h1>
+                        <h3>Please make sure your words are spelled correctly or use less or different keywords.</h3>
+                    </div>
 
             }
         </div>
